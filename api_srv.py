16b990dc5f39b12ff03c01a725f54a06640a7681
jerry <jerry@uinnova.com>
2017-08-16 11:24:52 -0700

    change port to 9009

--- api_srv.py
+++ api_srv.py
@@ -43,4 +43,4 @@ if __name__ == '__main__':
     handler.setFormatter(formatter)
     app.logger.addHandler(handler)
     CORS(app)
-    app.run(host='0.0.0.0', port=9006, debug=True)
+    app.run(host='0.0.0.0', port=9009, debug=True)
